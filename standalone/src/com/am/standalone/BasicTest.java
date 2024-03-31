d202c6cbc8364878e3d14b402fa45c6a79d2487d
AM-19 <anubhavm.mishra19@gmail.com>
2024-03-31 23:41:24 +0530

    BasicTest

---
+++ standalone/src/com/am/standalone/BasicTest.java
@@ -0,0 +1,12 @@
+package com.am.standalone;
+
+import java.util.Scanner;
+
+public class Basic {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		System.out.println(x);
+	}
+}
